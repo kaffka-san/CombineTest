78552e3a9df2125d64e265c21e24ede6a0bbd719
Kaffka <kaffka.anastasia@gmail.com>
2023-08-15 12:53:58 +0200

    Initial Commit

---
+++ CombineTest/CombineTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTestApp.swift
+//  CombineTest
+//
+//  Created by Anastasia Lenina on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CombineTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
